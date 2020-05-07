699267ca5cac831f6dd79df368ee9adb0e99c252
Paweł Kownacki <koowal@gmail.com>
2020-05-07 22:19:37 +0100

    functional

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+const { series, src, dest, task, watch } = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 
-exports.default = defaultTask
+const bs = function (cb) {
+  browserSync.init({
+    server: {
+      baseDir: "./build",
+    },
+  });
+  cb();
+};
+
+const buildSass = function (cb) {
+  src("src/sass/*.scss").pipe(sass()).pipe(dest("build"));
+  cb();
+};
+
+const watchFiles = series(bs, buildSass, function () {
+  watch("src/sass/*.scss", buildSass);
+  watch("build/*.html").on("change", browserSync.reload);
+  watch("build/*.css").on("change", browserSync.reload);
+});
+
+exports.watchFiles = watchFiles;
+exports.bs = bs;
+exports.buildSass = buildSass;
+exports.default = watchFiles;
